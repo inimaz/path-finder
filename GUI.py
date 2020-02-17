7118de40983db3d31729c292c2f5010c54f1dbde
inigo imaz chacon <inigo.imazchacon@amadeus.com>
2020-02-17 17:47:20 +0100

    UI working with numbers

--- GUI.py
+++ GUI.py
@@ -16,12 +16,15 @@ class StartUI:
         self.x0, self.y0, self.xf, self.yf = -1, -1, -1, -1
 
         self.Field = Full_field()
-
-        # self.__draw_obstacles()
-
-        # Initialize the GUI
         self.master = master
         master.title("Path finder")
+
+        # Initialize the GUI
+        try:
+            self.canvas.destroy()
+        except:
+            pass
+
         self.canvas = Canvas(master, width=WIDTH, height=HEIGHT, bg="white")
         self.canvas.pack(fill=BOTH, side=BOTTOM)
 
@@ -55,24 +58,23 @@ class StartUI:
 
             self.start_button = Button(
                 self.master, text="Find the shortest path",
-                fg='purple', command=self.__start_path_finding)
+                fg='green', command=self.__start_path_finding)
             self.start_button.pack(side=BOTTOM)
 
         self.__draw_box()
 
     def __start_path_finding(self):
+        self.label.config(text="Finding the shortest path...")
+        self.start_button.config(text="Restart", command=self.__init__(self.master))
         if self.Field.status[self.xf, self.yf] != 1:
             self.Field.solve_one_step()
             self.__draw_box()
-            time.sleep(1)
             self.Field.min_closest_node()
             self.__draw_box()
-            self.canvas.after(700, self.__start_path_finding)
+            self.canvas.after(200, self.__start_path_finding)
         else:
             self.Field.show_path()
             self.__draw_box()
-
-        
 
     def __draw_obstacles(self):
         if self.selected_row != -1 and self.selected_column != -1:
@@ -94,7 +96,7 @@ class StartUI:
                 if self.Field.status[row, column] == 0:
                     color = "white"
                 elif self.Field.status[row, column] == 1:
-                    color = "blue"
+                    color = "light grey"
                 elif self.Field.status[row, column] == 2:
                     color = "yellow"
                 elif self.Field.status[row, column] == -1:
@@ -109,6 +111,22 @@ class StartUI:
                 y1 = MARGIN + (column + 1) * SIDE - 1
                 self.canvas.create_rectangle(
                     x0, y0, x1, y1, fill=color, outline="black", tags="cells")
+        if self.x0 != -1 and self.y0 != -1:
+            x0 = MARGIN + self.x0 * SIDE + 1
+            y0 = MARGIN + self.y0 * SIDE + 1
+            x1 = MARGIN + (self.x0 + 1) * SIDE - 1
+            y1 = MARGIN + (self.y0 + 1) * SIDE - 1
+            self.canvas.create_rectangle(
+                x0, y0, x1, y1, outline="red")
+
+        if self.xf != -1 and self.yf != -1:
+            x0 = MARGIN + self.xf * SIDE + 1
+            y0 = MARGIN + self.yf * SIDE + 1
+            x1 = MARGIN + (self.xf + 1) * SIDE - 1
+            y1 = MARGIN + (self.yf + 1) * SIDE - 1
+            self.canvas.create_rectangle(
+                x0, y0, x1, y1, outline="green")
+        self.__draw_distance_value()
 
     def canvas_click(self, event):
         '''
@@ -133,14 +151,25 @@ class StartUI:
         '''
         Draw red box around the cell selected
         '''
-        self.canvas.delete("red_square")
+        self.canvas.delete("purple_square")
         if self.selected_row >= 0 and self.selected_column >= 0:
             x0 = MARGIN + self.selected_row * SIDE + 1
             y0 = MARGIN + self.selected_column * SIDE + 1
             x1 = MARGIN + (self.selected_row + 1) * SIDE - 1
             y1 = MARGIN + (self.selected_column + 1) * SIDE - 1
             self.canvas.create_rectangle(
-                x0, y0, x1, y1, outline="red", tags="red_square")
+                x0, y0, x1, y1, outline="purple", tags="purple_square")
+
+    def __draw_distance_value(self):
+        for i in range(0, cells_number):
+            for j in range(0, cells_number):
+                number = self.Field.values[i, j]
+                if number != 0:
+                    number = "{:.1f}".format(number)
+                    x = MARGIN + SIDE / 2 + i * SIDE
+                    y = MARGIN + SIDE / 2 + j * SIDE
+                    self.canvas.create_text(
+                        x, y, text=number, tags="numbers", fill="black")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import numpy as np
 import random
+import logging
+import logging.config
 
+logging.config.fileConfig('logging.conf')
+
+# create logger
+logger = logging.getLogger('simpleExample')
+
+
+# Setting the size of the field
 cells_number = 15
 
 
 """
 We are going to show the following path finding algorithms:
-Dijkstra’s Algorithm
-A* Search Algorithm
-D* Algorithm
+Dijkstra’s Algorithm algorithm = D
+A* Search Algorithm algorithm = A*
+D* Algorithm algorithm = D*
 """
 
 
@@ -22,9 +31,10 @@ class Full_field():
         -2 = Obstacle
         3 = shortest path
         '''
+        self.algorithm = "A*"
         self.__start_field()
-        print(self.status)
-        print(self.values)
+        logger.info('Starting status of the board \n:' + str(self.status))
+        logger.info('Starting distance values\n' + str(self.values))
 
     def __start_field(self):
         self.values = np.zeros((cells_number, cells_number))
@@ -42,7 +52,7 @@ class Full_field():
             while _ < number_of_obstacles:
                 i = random.randint(0, cells_number - 1)
                 j = random.randint(0, cells_number - 1)
-                if self.status[i, j] != 1:
+                if self.status[i, j] not in [1, 2]:
                     self.status[i, j] = -2
                 _ += 1
         else:
@@ -66,12 +76,22 @@ class Full_field():
 
         self.status[xf, yf] = 2
 
+    def select_algorithm(self, algorithm):
+        """
+        We are going to show the following path finding algorithms:
+                Dijkstra’s Algorithm algorithm = Di
+                A* Search Algorithm algorithm = A*
+                D* Algorithm algorithm = D*
+        """
+        self.algorithm = algorithm
+
     def find_shortest_path(self):
         while self.status[self.xf, self.yf] != 1:
             self.check_nodes()
             self.min_closest_node()
-            print('These are the values\n', self.values)
-            print('This is to see which one is solved\n', self.status)
+            logger.info('These are the values\n' + str(self.values))
+            logger.info('This is to see which one is solved\n' +
+                        str(self.status))
 
     def check_nodes(self):
 
@@ -79,7 +99,7 @@ class Full_field():
             for j in range(cells_number):
                 if self.status[i, j] == 1:
                     self.find_next_node(i, j)
-        print("These are the candidates'status\n", self.status)
+        logger.info("These are the candidates'status\n" + str(self.status))
 
     def min_closest_node(self):
         # Search the closest node with the minimum value
@@ -91,10 +111,14 @@ class Full_field():
                     self.status[i, j] = 0
                     node_values.append(self.values[i, j])
                     node_pos.append([i, j])
-        print(node_values)
-        pos = np.argmin(node_values)
+        logger.debug('This is the minimum value: ' + str(node_values))
+        try:
+            pos = np.argmin(node_values)
+        except:
+            logger.error('It is not possible to go from ' + str(self.x0) + str(self.y0) +
+                         " to " + str(self.xf) + str(self.yf))
         closest_node = tuple(node_pos[pos])
-        print('Node to be set as solved', closest_node)
+        logger.debug('Node to be set as solved' + str(closest_node))
 
         self.status[closest_node] = 1
 
@@ -108,17 +132,29 @@ class Full_field():
                     self.__set_value(x0, y0, x1, y1)
 
     def __set_value(self, x0, y0, x1, y1):
-        distance = ((x1 - x0) ** 2 + (y1 - y0) ** 2) ** (1 / 2)
-        cumulative_distance = self.values[x0, y0] + distance
-        if (self.status[x1, y1] in [0, 2]) | \
-                (self.status[x1, y1] == -1 &
+        distance_to_origin = self.__distance_between(x0, y0, x1, y1)
+        if self.algorithm == "A*":
+            distance_to_destination_x0 = self.__distance_between(
+                x0, y0, self.xf, self.yf)
+            distance_to_destination = self.__distance_between(
+                x1, y1, self.xf, self.yf)
+        elif self.algorithm == "Di":
+            distance_to_destination = distance_to_destination_x0 = 0
+        cumulative_distance = self.values[x0, y0] - distance_to_destination_x0 + \
+            distance_to_origin + distance_to_destination
+        if (self.status[x1, y1] in [0, 2]) or \
+                ((self.status[x1, y1] == -1) and
                  (cumulative_distance < self.values[x1, y1])):
             self.values[x1, y1] = cumulative_distance
             self.status[x1, y1] = -1
             self.previous_cell[x1, y1] = (x0, y0)
 
+    def __distance_between(self, x0, y0, x1, y1):
+        distance = ((x1 - x0) ** 2 + (y1 - y0) ** 2) ** (1 / 2)
+        return distance
+
     def show_path(self):
-        print(self.previous_cell)
+        logger.debug('This is the previous_cell\n' + str(self.previous_cell))
         x = self.xf
         y = self.yf
 
@@ -128,8 +164,8 @@ class Full_field():
                 self.status[x, y] = 2
             x, y = self.previous_cell[x, y]
 
-        print("This is the shortest path from ", self.x0, self.y0,
-              "to", self.xf, self.yf, "\n", self.status)
+        logger.info("This is the shortest path from " + str(self.x0) + str(self.y0) +
+                    " to " + str(self.xf) + str(self.yf) + "\n" + str(self.status))
 
     def solve_fast(self):
         self.find_shortest_path()
@@ -138,7 +174,6 @@ class Full_field():
     def solve_one_step(self):
         if self.status[self.xf, self.yf] != 1:
             self.check_nodes()
-            
 
 
 class Find_Path(Full_field):
